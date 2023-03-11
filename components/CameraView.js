f4289c2c84849711fc21c64695c619e4715e2f03
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-03-11 15:18:41 +0530

    added slider thumb image

--- components/CameraView.js
+++ components/CameraView.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Camera,CameraType } from 'expo-camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { StyleSheet,View,Text,Vibration } from 'react-native';
 import { useGlobalContext } from '../context';
 import { useIsFocused } from '@react-navigation/native';
 import QrCodeContainer from './QrCodeContainer';
-import Slider from '@react-native-community/slider';
-import ZoomIcon from 'react-native-vector-icons/MaterialIcons'
+import ZoomControlSlider from './ZoomControlSlider';
+
+
 
 
 const CameraView = ({ navigation })=> {
@@ -14,28 +15,6 @@ const CameraView = ({ navigation })=> {
     const {isFlashLightOn} = useGlobalContext()
     const isFocused = useIsFocused();
     const [zoom,setZoom] = useState(0)
-    const [sliderThumbIcon,setSliderThumbIcon] = useState()
-
-
-    const fetchIconImage =  ()=> {
-        try {
-            Promise.all([
-                ZoomIcon.getImageSource('zoom-in',25,'gold')
-            ]).then(source => {
-                console.log(source)
-            })
-        }
-        catch (e) {
-            console.log(e)
-        }
-    }
-    
-
-    // useEffect(()=>{
-    //     fetchIconImage()
-    // },[])
-
-
 
 
     const getCurrentDate = ()=> {
@@ -90,32 +69,21 @@ const CameraView = ({ navigation })=> {
                     type={CameraType.back}
                     zoom={zoom}
                 >
-                    {/* <View style={styles.outerQrZone}/> */}
                     <QrCodeContainer />
-                    {/* <View style={styles.outerQrZone}/> */}
                 </Camera>
             }  
-            <View style={styles.zoomControlSlider}>      
-                <Slider
-                    style={styles.slider}
-                    minimumValue={0}
-                    maximumValue={1}
-                    value={zoom}
-                    onValueChange={(value)=>setZoom(value)}
-                    minimumTrackTintColor='gold'
-                    thumbTintColor='gold'
-                    // step={0.1}
-                    // thumbImage={sliderThumbIcon}
-                />  
-            </View>
+           
+            <ZoomControlSlider zoom={zoom} setZoom={setZoom}/>
+
             <View style={styles.zoomTextView}>
-                    <Text style={styles.zoomText}>Zoom : {
-                        zoom <= 0.05 ? 0 : zoom.toFixed(1)
-                    }</Text>
+                <Text style={styles.zoomText}>Zoom : {
+                    zoom <= 0.05 ? 0 : zoom.toFixed(1)
+                }</Text>
             </View>     
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     cameraContainer : {
@@ -129,26 +97,9 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'center',
     },  
-    zoomControlSlider : {
-        position:'absolute',
-        top:'80%',
-        marginLeft:'18%',
-        backgroundColor:'#181818',
-        borderRadius:50,
-        flexDirection:'row',
-        alignItems:'center',
-        gap:20,
-    },
-    slider : {
-        paddingHorizontal:25,
-        paddingVertical:25,
-        color:'gold',
-        height:20,
-        width:250,
-    },
     zoomTextView : {  
         position:'absolute',
-        top:'89%',
+        top:'90%',
         left:'43%',
     },  
     zoomText : { 
---
+++ components/ZoomControlSlider.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View,StyleSheet } from 'react-native'
+import Slider from '@react-native-community/slider';
+import zoomInImage from '../assets/zoomIcon.png'
+
+const ZoomControlSlider = ({ zoom,setZoom })=> {
+    return (
+        <View style={styles.zoomControlSlider}>      
+            <Slider
+                style={styles.slider}
+                minimumValue={0}
+                maximumValue={1}
+                value={zoom}
+                onValueChange={(value)=>setZoom(value)}
+                minimumTrackTintColor='gold'
+                maximumTrackTintColor='grey'
+                thumbImage={zoomInImage}
+                thumbTintColor='yellow'
+            />  
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    zoomControlSlider : {
+        position:'absolute',
+        top:'80%',
+        marginLeft:'17%',
+        backgroundColor:'#181818',
+        borderRadius:50,
+    },
+    slider : {
+        marginLeft:15,
+        paddingVertical:25,
+        color:'gold',
+        height:20,
+        width:235,
+    },
+})
+export default ZoomControlSlider
